381df9f048752d3dff11d6a5428949ec51598d4c
Jabulani Msimango <jmsimang@student.wethinkcode.co.za>
2019-03-26 00:38:01 +0200

    Request driver updated to view firefox as headless

--- weatherviaterminal/core/request.py
+++ weatherviaterminal/core/request.py
@@ -1,5 +1,5 @@
 import os
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+# from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
 
@@ -8,13 +8,15 @@ class Request:
     def __init__(self, base_url):
         self._capabilities = webdriver.DesiredCapabilities().FIREFOX
         self._capabilities["marionette"] = True
-        self._gecko_binary = os.path.join(os.curdir, 'firefox/geckodriver')
-        self._firefox_binary = FirefoxBinary(r'/usr/local/bin/firefox')
+        self._options = webdriver.FirefoxOptions()
+        self._options.add_argument('-headless')
+        # self._gecko_binary = os.path.join(os.curdir, 'firefox/geckodriver')
+        # self._firefox_binary = FirefoxBinary(r'/usr/local/bin/firefox')
         self._base_url = base_url
-        self._options = Options()
-        self._options.headless = True
-        self._driver = webdriver.Firefox(capabilities=self._capabilities,
-                                         firefox_options=self._options)
+        # self._options = Options()
+        # self._options.headless = True
+        self._driver = webdriver.Firefox(options=self._options,
+                                         capabilities=self._capabilities)
 
     def _fetch_data(self, forecast, area):
         url = self._base_url.format(forecast=forecast, area=area)
